085a55e67c0c59e89275a120db865952c68a312f
pwong <nyphilwong@gmail.com>
2025-11-23 00:33:39 -0500

    added editor function to auto size ghostty panes and open gemini

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,9 @@
-# Alias
+# Aliases
 alias ll="ls -lah"
 alias rm="rm -vi"
 
-alias venv='python -m venv .venv'
-alias activate='source .venv/bin/activate'
+alias venv="python -m venv .venv"
+alias activate="source .venv/bin/activate"
+
+# ghostty editor
+alias editor="~/.config/ghostty/scripts/editor"
