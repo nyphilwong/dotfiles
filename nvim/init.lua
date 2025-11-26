45d8b582ee0417b4bfacfed37ac2cc944b6ec0a6
pwong <nyphilwong@gmail.com>
2025-11-25 22:00:38 -0500

    add treesitter and clean up local theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,8 +24,9 @@ local plugins = {
   { "projekt0n/github-nvim-theme", name = "github-theme"},
   {
     'nvim-telescope/telescope.nvim', tag = 'v0.1.9',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+     dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
 }
 local opts = {}
 
@@ -35,5 +36,20 @@ local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- needs ripgrep (brew install ripgrep)
 
-require("github-theme").setup()
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "vim", "python", "javascript"},
+  highlight = {enable = true},
+  indent = {enable = true}
+})
+
+local theme = require("github-theme")
+theme.setup({
+  options = {
+    styles = { comments = 'italic' },
+    modules = {
+      treesitter = true,
+    }
+  }
+})
 vim.cmd.colorscheme "github_dark_dimmed"
